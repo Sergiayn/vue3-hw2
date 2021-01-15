08a70e4b23975ad7b52fe99eebd0660bb1cd662e
Sergey <SergeySm128@gmail.com>
2021-01-15 19:39:16 +0200

    add components CommentCard and FormCard

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="container column">
-    <form class="card card-w30">
-      <div class="form-control">
-        <label for="type">Тип блока</label>
-        <select id="type">
-          <option value="title">Заголовок</option>
-          <option value="subtitle">Подзаголовок</option>
-          <option value="avatar">Аватар</option>
-          <option value="text">Текст</option>
-        </select>
-      </div>
-
-      <div class="form-control">
-        <label for="value">Значение</label>
-        <textarea id="value" rows="3"></textarea>
-      </div>
-
-      <button class="btn primary">Добавить</button>
-    </form>
+    <FormCard/>
 
     <div class="card card-w70">
       <h1>Резюме Nickname</h1>
@@ -31,28 +14,18 @@
       <h3>Добавьте первый блок, чтобы увидеть результат</h3>
     </div>
   </div>
+
   <div class="container">
-    <p>
-      <button class="btn primary">Загрузить комментарии</button>
-    </p>
-    <div class="card">
-      <h2>Комментарии</h2>
-      <ul class="list">
-        <li class="list-item">
-          <div>
-            <p><strong>test@microsoft.com</strong></p>
-            <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="loader"></div>
+    <CommentCard/>
   </div>
 </template>
 
 <script>
-export default {
+import CommentCard from "@/components/CommentCard"
+import FormCard from "@/components/FormCard"
 
+export default {
+  components: {CommentCard, FormCard}
 }
 </script>
 
