dcb6a9feda0c6b4a2ecae31c6ad834efd9352765
Sergey <SergeySm128@gmail.com>
2021-01-17 12:56:01 +0200

    implement component CommentCard

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -1,27 +1,44 @@
 <template>
   <p>
-    <button class="btn primary">Загрузить комментарии</button>
+    <button class="btn primary" @click.prevent="loadComments">Загрузить комментарии</button>
   </p>
-  <div class="card">
+  <div class="card" v-if="comments.length">
     <h2>Комментарии</h2>
     <ul class="list">
-      <li class="list-item">
+      <li class="list-item" v-for="comment in comments" :key="comment.id">
         <div>
-          <p><strong>test@microsoft.com</strong></p>
-          <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
+          <h3>{{comment.name}}</h3>
+          <p><strong>{{comment.email}}</strong></p>
+          <small>{{comment.body}}</small>
         </div>
       </li>
     </ul>
   </div>
-  <div class="loader"></div>
+  <div class="loader" v-if="activeLoader"></div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'CommentCard',
+  created() {
+    axios.defaults.baseURL = this.baseUrl
+  },
+  data() {
+    return {
+      activeLoader: false,
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      comments: []
+    }
+  },
+  methods: {
+    async loadComments() {
+      this.activeLoader = true
+      const response = await axios.get('comments?_limit=42')
+
+      this.comments = response.data
+      this.activeLoader = false
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
