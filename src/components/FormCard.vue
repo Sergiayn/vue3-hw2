66fa4caa07c8055886acc3b3f94e48d237e3f49f
Sergey <SergeySm128@gmail.com>
2021-01-20 09:23:42 +0200

    move formValidator to separate file

--- src/components/FormCard.vue
+++ src/components/FormCard.vue
@@ -34,28 +34,7 @@
 
 <script>
 
-const formValidator = (value, params) => {
-  const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  let errors = []
-  let result = {}
-
-  if(value.length < 4) {
-    errors.push({
-      title:'Поле должно содержать минимум 4 символа',
-      type: 'danger'
-    });
-  }
-  if(params.activeType === 'ComponentEmail' && !regexEmail.test(value)){
-    errors.push({
-      title:'Укажите корректный адрес электронной почты.',
-      type: 'warning'
-    });
-  }
-  result.errors = errors
-  result.status = !errors.length
-  return result
-}
-
+import {formValidator} from '@/validators/formValidator'
 export default {
   name: 'FormCard',
   mounted() {
